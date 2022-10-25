f309c47e160f985b46f46752e9b9132ec1a5f384
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2022-10-25 12:12:49 -0500

    Camera instance initialized

--- arachnid_object/camera.js
+++ arachnid_object/camera.js
@@ -1,10 +1,10 @@
 export default class Camera {
 
-    #data = new Uint16Array(4);
+    #data = new ArrayBuffer()
 
     constructor(gl, size) {
         this.gl = gl;
         this.#data.set(size, 2);
     }
-    
+
 }
